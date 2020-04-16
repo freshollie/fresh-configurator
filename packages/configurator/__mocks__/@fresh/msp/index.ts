f1c998b1b1a9f4615882cfaccc885eef4c73c0a0
Oliver Bell <freshollie@gmail.com>
2020-04-16 19:41:23 +0100

    chore(configurator): remove msp mock

--- packages/configurator/__mocks__/@fresh/msp/index.ts
+++
@@ -1,9 +0,0 @@
-export const isOpen = (): boolean => false;
-export const open = (): Promise<boolean> => Promise.resolve(true);
-export const ports = (): Promise<string[]> => Promise.resolve([]);
-export const getAttitude = (): Promise<object> =>
-  Promise.resolve({
-    roll: 0,
-    pitch: 0,
-    heading: 0,
-  });
