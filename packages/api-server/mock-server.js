3e0f8396ca91ae5dc6f467721b5edaa4e823a572
Oliver Bell <freshollie@gmail.com>
2020-04-28 15:32:34 +0100

    chore(api-server): fix mock server

--- packages/api-server/mock-server.js
+++ packages/api-server/mock-server.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-unresolved */
-const { createServer } = require("./dist");
+const { createServer } = require("./dist/cjs");
 
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 9000;
 console.log(`starting mock server on ${port}`);
