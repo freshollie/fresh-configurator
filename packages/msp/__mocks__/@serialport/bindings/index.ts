a5bdeb539772bf1059e1caa8d7283dc855ca3c9b
Oliver Bell <freshollie@gmail.com>
2020-04-28 11:47:02 +0100

    chore(msp): fix tests

--- packages/msp/__mocks__/@serialport/bindings/index.ts
+++ packages/msp/__mocks__/@serialport/bindings/index.ts
@@ -2,4 +2,4 @@ import MockBindings from "@serialport/binding-mock";
 
 // Trick our tests into using mock bindings for the
 // serial port bindings, rather than the real bindings
-export default MockBindings;
+export = MockBindings;
