2a29a2cd45f2007bb8010b6f5deb1ac63aa7d026
Oliver Bell <freshollie@gmail.com>
2020-05-05 03:38:31 +0100

    tests(configurator): added storysnaps

--- packages/configurator/storyshots.runner.ts
+++ packages/configurator/storyshots.runner.ts
@@ -5,8 +5,17 @@ import path from "path";
 initStoryshots({
   suite: "Component snapshots",
   test: imageSnapshot({
+    chromeExecutablePath: process.env.PUPPETEER_EXEC_PATH,
     storybookUrl: !process.env.STORYBOOK_PORT
-      ? `file://${path.resolve(__dirname, "../storybook-static")}`
+      ? `file://${path.resolve(__dirname, "storybook-static")}`
       : `http://localhost:${process.env.STORYBOOK_PORT}`,
+    getMatchOptions: () => ({
+      failureThreshold: 2,
+      failureThresholdType: "pixel",
+    }),
+    getScreenshotOptions: () => ({ encoding: "base64", omitBackground: true }),
+    customizePage: (page) => page.setViewport({ width: 960, height: 640 }),
+    afterScreenshot: ({ context: { kind, story } }) =>
+      process.stdout.write(`Tested ${kind}:${story}\n`),
   }),
 });
