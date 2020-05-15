d113a842798c76e865a6b86bd8807d68df54962f
Oliver Bell <freshollie@gmail.com>
2020-05-16 00:26:58 +0100

    fix(configurator): fix storybook build

--- packages/configurator/.storybook/main.js
+++ packages/configurator/.storybook/main.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-param-reassign */
 const path = require("path");
+const { NormalModuleReplacementPlugin } = require("webpack");
 
 module.exports = {
   stories: ["../stories/**/*.stories.tsx"],
@@ -55,6 +56,12 @@ module.exports = {
       ],
     });
 
+    config.plugins.push(
+      new NormalModuleReplacementPlugin(
+        /\.graphql$/,
+        path.resolve(__dirname, "../src/gql/__generated__/index.tsx")
+      )
+    );
     return config;
   },
 };
