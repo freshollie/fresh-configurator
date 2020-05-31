5fef163d7db9dbb6923ddf7e2d2767b924d17a48
Oliver Bell <freshollie@gmail.com>
2020-05-31 23:52:59 +0100

    feat(api): added more read board info tests, added readUID, added writeReboot

---
+++ packages/api/test/writeReboot.spec.ts
@@ -0,0 +1,31 @@
+import mockMsp from "./mockMsp";
+import { writeReboot, RebootTypes } from "../src";
+import codes from "../src/codes";
+
+describe("writeReboot", () => {
+  it("should return true if the reboot request was successful for 1.40.0", async () => {
+    mockMsp.setResponse([44, 0, 62, 0, 12, 87, 52, 87, 52, 54, 48, 32]);
+    mockMsp.setApiVersion("1.40.0");
+
+    expect(await writeReboot("/dev/somedevice")).toBeTruthy();
+    expect(mockMsp.execute).toHaveBeenCalledWith("/dev/somedevice", {
+      code: codes.MSP_SET_REBOOT,
+      data: undefined,
+      timeout: 3000,
+    });
+  });
+
+  it("should allow a type of reboot to be given", async () => {
+    mockMsp.setResponse([44, 0, 62, 0, 12, 87, 52, 87, 52, 54, 48, 32]);
+    mockMsp.setApiVersion("1.40.0");
+
+    expect(
+      await writeReboot("/dev/somedevice", RebootTypes.BOOTLOADER)
+    ).toBeTruthy();
+    expect(mockMsp.execute).toHaveBeenCalledWith("/dev/somedevice", {
+      code: codes.MSP_SET_REBOOT,
+      data: [RebootTypes.BOOTLOADER],
+      timeout: 3000,
+    });
+  });
+});
