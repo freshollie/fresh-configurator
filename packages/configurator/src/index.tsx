09b6f07e613d746d5f4337a1c9453c913a4aaa60
Oliver Bell <freshollie@gmail.com>
2021-01-01 18:46:19 +0000

    feat(configurator): added error boundary for app crash

--- packages/configurator/src/index.tsx
+++ packages/configurator/src/index.tsx
@@ -1,17 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloProvider } from "@apollo/client";
+import { ErrorBoundary, FallbackProps } from "react-error-boundary";
 import App from "./App";
 import client from "./gql/client";
 import { ThemeProvider } from "./theme";
 
+const Fallback: React.FC<FallbackProps> = ({ error, resetErrorBoundary }) => (
+  <div
+    role="alert"
+    style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+    }}
+  >
+    <h1>Something went wrong :(</h1>
+    <pre>{error.stack}</pre>
+    <button
+      type="button"
+      onClick={async () => {
+        await client.resetStore();
+        resetErrorBoundary();
+      }}
+    >
+      Reload
+    </button>
+  </div>
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </ApolloProvider>
+    <ErrorBoundary FallbackComponent={Fallback}>
+      <ApolloProvider client={client}>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </ApolloProvider>
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById("app")
 );
