4c7d10b51a1223560144716b2130dbaded2afb0c
Oliver Bell <freshollie@gmail.com>
2021-03-15 22:36:02 +0000

    feat(api-server): provide option for legacy websocket protocol

--- packages/api-server/start.ts
+++ packages/api-server/start.ts
@@ -1,7 +1,10 @@
 import { createServer } from "./src";
 
 (async () => {
-  await createServer().listen({ port: 9000, hostname: "127.0.0.1" });
+  await createServer({ legacyWsProtocol: true }).listen({
+    port: 9000,
+    hostname: "127.0.0.1",
+  });
   // eslint-disable-next-line no-console
   console.log("listening");
 })();
