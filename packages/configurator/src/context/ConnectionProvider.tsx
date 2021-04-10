90e30b21b01ace52df2ad3d29c099a79ad85f6e8
Oliver Bell <freshollie@gmail.com>
2021-04-10 18:34:50 +0100

    feat(configurator): disable arming when configuration loaded

--- packages/configurator/src/context/ConnectionProvider.tsx
+++ packages/configurator/src/context/ConnectionProvider.tsx
@@ -1,12 +1,14 @@
 import { useLocation } from "wouter";
-import React, { createContext, useEffect } from "react";
-import { gql, useSubscription } from "../gql/apollo";
+import React, { createContext, useEffect, useState } from "react";
+import { Flex, Spinner, Box, Text, Set } from "bumbag";
+import { gql, useMutation, useSubscription } from "../gql/apollo";
 import useLogger from "../hooks/useLogger";
 
 export const ConnectionContext = createContext("");
 
 const ConnectionProvider: React.FC = ({ children }) => {
   const log = useLogger();
+  const [connected, setConnected] = useState(false);
   const [location, setLocation] = useLocation();
   const connectionId = location.split("/connections/").pop()?.split("/")[0];
 
@@ -44,16 +46,82 @@ const ConnectionProvider: React.FC = ({ children }) => {
     }
   );
 
-  // Navigate away if the connection is not valid
+  const [disconnect, { loading: disconnecting }] = useMutation(
+    gql`
+      mutation Disconnect($connection: ID!) {
+        close(connectionId: $connection)
+      }
+    ` as import("@graphql-typed-document-node/core").TypedDocumentNode<
+      import("./__generated__/ConnectionProvider").DisconnectMutation,
+      import("./__generated__/ConnectionProvider").DisconnectMutationVariables
+    >,
+    {
+      variables: {
+        connection: connectionId,
+      },
+    }
+  );
+
+  const [disableArming] = useMutation(
+    gql`
+      mutation DisableArming($connection: ID!) {
+        deviceSetArming(
+          connectionId: $connection
+          armingDisabled: true
+          runawayTakeoffPreventionDisabled: false
+        )
+      }
+    ` as import("@graphql-typed-document-node/core").TypedDocumentNode<
+      import("./__generated__/ConnectionProvider").DisableArmingMutation,
+      import("./__generated__/ConnectionProvider").DisableArmingMutationVariables
+    >,
+    {
+      variables: {
+        connection: connectionId,
+      },
+      onCompleted: () => {
+        log("<b>Arming disabled</b>");
+        setConnected(true);
+      },
+      onError: async (e) => {
+        log(
+          `<span class="message-negative">Error disabling arming: ${e.message}</span>`
+        );
+        await disconnect().catch((disconnectError) => {
+          log(`Error disconnecting, ${disconnectError.message}`);
+        });
+        setLocation("/");
+      },
+    }
+  );
+
   useEffect(() => {
     if (error) {
+      // Navigate away if the connection is not valid
       log("Invalid connectionId");
       setLocation("/");
+    } else if (connectionId) {
+      // Otherwise, disable arming for this connection
+      disableArming();
     }
-  }, [error, log, setLocation]);
+  }, [error, log, setLocation, disableArming, connectionId]);
 
-  if (!connectionId) {
-    return null;
+  if (!connectionId || disconnecting || !connected) {
+    return (
+      <Flex
+        width="100%"
+        height="100vh"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box>
+          <Set>
+            <Text>Configuring connection</Text>
+            <Spinner size="large" />
+          </Set>
+        </Box>
+      </Flex>
+    );
   }
 
   return (
