fb7c5a7e7ac78fb8c0ba4ea08869fab6d23ebe0b
Oliver Bell <freshollie@gmail.com>
2021-04-11 00:07:11 +0100

    chore(configurator): throw an error if connection hook used without context

--- packages/configurator/src/hooks/useConnection.ts
+++ packages/configurator/src/hooks/useConnection.ts
@@ -1,4 +1,13 @@
 import { useContext } from "react";
 import { ConnectionContext } from "../context/ConnectionProvider";
 
-export default (): string => useContext(ConnectionContext);
+export default (): string => {
+  const connection = useContext(ConnectionContext);
+  if (!connection) {
+    throw new Error(
+      "No connection, make sure your component is wrapped in ConnectionProvider"
+    );
+  }
+
+  return connection;
+};
