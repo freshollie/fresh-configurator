6b2378374f73ecc5f73ee5c3ab4667e74cfbadbd
Oliver Bell <freshollie@gmail.com>
2021-11-05 12:57:08 +0000

    chore(pipeline): update puppeteer container (#60)

--- tools/storyshots-runner/runner.js
+++ tools/storyshots-runner/runner.js
@@ -89,7 +89,7 @@ const run = async ({
   // Launch Puppeteer process to fetch stories info.
   const storiesBrowser = await new StoriesBrowser(connection, {
     launchOptions: {
-      executablePath: CI ? process.env.PUPPETEER_EXECUTABLE_PATH : undefined,
+      executablePath: CI ? process.env.PUPPETEER_EXEC_PATH : undefined,
       args: ["--no-sandbox"],
     },
   }).boot();
@@ -117,9 +117,7 @@ const run = async ({
     new Array(numWorkers).fill(0).map((i) =>
       new StoryPreviewBrowser(connection, i, {
         launchOptions: {
-          executablePath: CI
-            ? process.env.PUPPETEER_EXECUTABLE_PATH
-            : undefined,
+          executablePath: CI ? process.env.PUPPETEER_EXEC_PATH : undefined,
           headless: true,
           args: ["--no-sandbox", "--disable-gpu", "--font-render-hinting=none"],
         },
