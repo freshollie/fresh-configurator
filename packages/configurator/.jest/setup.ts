3115e8cb040eed69ea1b0ebfbcf5801beefa1e77
Oliver Bell <freshollie@gmail.com>
2021-11-26 21:22:44 +0000

    fixes

--- packages/configurator/.jest/setup.ts
+++ packages/configurator/.jest/setup.ts
@@ -4,6 +4,15 @@ import "@testing-library/jest-dom";
 jest.mock("../src/renderer/components/Icon");
 jest.mock("../src/renderer/flightindicators/assets");
 jest.mock("../src/renderer/logos");
-jest.mock("../src/workers/SchemaBackend.bootstrap", () => ({
-  default: Worker,
-}));
+jest.mock("../src/workers/SchemaBackend.bootstrap", () => {
+  const mockWorker = {
+    onmessage: () => {},
+    postMessage: (message: Record<string, unknown>) => {
+      if ("type" in message && message.type === "init") {
+        mockWorker.onmessage?.({ data: { type: "ready" } } as MessageEvent);
+      }
+    },
+  } as unknown as Worker;
+
+  return mockWorker;
+});
